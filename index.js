679a0f14915db6467e17abae654a47e4cddcc9f3
root27 <oguzhaneee@gmail.com>
2022-11-29 00:06:02 +0300

    added server

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+
+const nodemailer = require('nodemailer');
+
+const handlebars = require('handlebars');
+
+const fs = require('fs');
+
+const multer = require('multer');
+
+const upload = multer({dest: 'uploads/'});
+
+
+
+app.use(express.json());
+
+app.use(cors({origin: 'http://localhost:3000'}));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post("/upload", upload.single("file"), (req, res) => {
+
+    const file = req.file;
+    console.log(file)
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+
+
+        auth: {
+            user: "oguzhaneee@gmail.com",
+            pass: "hphmpdvsejfgntie"
+        }
+    });
+
+    const filestream = fs.createReadStream(file.path);
+
+    
+
+    // const source = filestream;
+
+    // const template = handlebars.compile(source);
+
+
+
+    const mailOptions = {
+        from: "oguzhaneee@gmail.com",
+        to: 'oguzhaneee@gmail.com',
+        subject: 'Sending Email using Node.js',
+        html: filestream,
+        
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    }
+    );
+
+
+
+
+
+
+})
+
+
+
+
+
+app.listen(3001, () => {
+    console.log("Server is running on port 5000");
+    }
+)
